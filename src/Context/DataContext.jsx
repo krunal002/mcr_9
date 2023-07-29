f0e30378f0d507036ee203ccbfb92c352a95a0fb
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-07-29 17:23:56 +0530

    watch later and other functions done

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -4,10 +4,27 @@ export const DataContext = createContext();
 
 export const DataContextHandler = ({ children }) => {
   const [ videoData, setVideoData ] = useState(videos)
+  const [ playlist, setPlaylist ] = useState([{newplaylist:[]}])
+
+  const addToWL = (videoId) => {
+    const updatedVideosData = videoData.map((video) =>
+      video._id === videoId ? { ...video, watchLater: true } : video
+    );
+    setVideoData(updatedVideosData);
+  };
+
+  const removeFromWL = (videoId) => {
+    console.log("added", videoId)
+    const updatedVideosData = videoData.map((video) =>
+      video._id === videoId ? { ...video, watchLater: false } : video
+    );
+    setVideoData(updatedVideosData);
+  };
+
   return (
     <div>
       <DataContext.Provider
-        value={{videoData, setVideoData}}
+        value={{ videoData, setVideoData, addToWL, removeFromWL }}
       > {children}</DataContext.Provider>
     </div>
   );
--- src/Pages/VideoDetails/Details.jsx
+++ src/Pages/VideoDetails/Details.jsx
@@ -1,6 +1,11 @@
-import "./Details.css"
+import "./Details.css";
 import { useContext } from "react";
 import { useParams } from "react-router";
+
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import WatchLaterOutlinedIcon from "@mui/icons-material/WatchLaterOutlined";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import EditNoteIcon from "@mui/icons-material/EditNote";
 
 import { DataContext } from "../../mcr_9";
 import Aside from "../../Components/Aside/Aside";
@@ -8,9 +13,10 @@ import MoreVideos from "../../Components/MoreVideos/MoreVideos";
 
 const Details = () => {
   const { videoId } = useParams();
-  const { videoData } = useContext(DataContext);
+  const { videoData, addToWL, removeFromWL } = useContext(DataContext);
 
   const VideoDetails = videoData.find(({ _id }) => _id === Number(videoId));
+  const isWL = VideoDetails.watchLater;
   return (
     <div className="details-container">
       <div className="left-container">
@@ -18,15 +24,45 @@ const Details = () => {
       </div>
 
       <div>
-        <iframe
-          title={VideoDetails._id}
-          width="560"
-          height="315"
-          src={VideoDetails.src}
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+        <div className="video-details-container">
+          <iframe
+            title={VideoDetails._id}
+            width="560"
+            height="315"
+            src={VideoDetails.src}
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+
+          <div className="details-info">
+            <div className="video-card-info">
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.Fe8Ph21Hbdb5TIYZmjCK2QHaFj&pid=Api&P=0&h=180"
+                alt="tempImage"
+                className="video-provider-Image"
+              />
+              <div className="video-card-details">
+                <span>{VideoDetails.title}</span>
+              </div>
+            </div>
+            <div className="detail-buttons">
+              <div
+                onClick={() =>
+                  isWL
+                    ? removeFromWL(VideoDetails._id)
+                    : addToWL(VideoDetails._id)
+                }
+              >
+                {isWL ? <WatchLaterIcon /> : <WatchLaterOutlinedIcon />}
+              </div>
+              <div><PlaylistAddIcon /></div>
+              <EditNoteIcon />
+            </div>
+          </div>
+        </div>
+
+        <h2>My Notes</h2>
       </div>
 
       <div>
